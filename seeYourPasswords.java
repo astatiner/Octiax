80eb1c4a6fae619ea44eb18ed3753e08b14db3d0
Encryptegral25 <77005352+Encryptegral25@users.noreply.github.com>
2021-07-28 17:09:26 +0530

    Update seeYourPasswords.java
    Modified the code a bit, added a function which allows the user to change their passwords now too.

--- seeYourPasswords.java
+++ seeYourPasswords.java
@@ -12,8 +12,7 @@ import javax.crypto.spec.SecretKeySpec;
 import javax.crypto.spec.IvParameterSpec;
 import org.apache.commons.codec.binary.Base64;
 
-public class seeYourPasswords
-{
+public class seeYourPasswords{
 	private static final String passWordKey = "it6.uJOf@2#MagD%", userNameKey = "hS3s*fU7&dkSp$&5", initVector = "*$SI9!7&yyrUjY@l",fileKey = "N!@&$k9nVLzoQOuW";
 	private static String sysName = System.getProperty("user.name");
 	public static void main(String[] args) throws Exception {
@@ -22,16 +21,45 @@ public class seeYourPasswords
 		dns.add(".org");
 		dns.add(".net");
 		dns.add(".io");
-		String orgUserName = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918";
-		String orgPassWord = "8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918";
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter your username");
-		String userName = sc.nextLine();
+		String userName = sc.next();
 		System.out.println("Enter your password");
-		String passWord = sc.nextLine();
+		String passWord = sc.next();
 		if (hashStrings(userName).equals(orgUserName) && hashStrings(passWord).equals(orgPassWord)) {
 			//getting the names of websites whose login credentials are stored
+			System.out.println("What do you want to do today?");
+			System.out.println("'SYP'- See your saved passwords");
+			System.out.println("'CU' - Change the username for a site");
+			System.out.println("'CP' - Change the password for a site");
+			String response = sc.next();
+			if(response.toLowerCase().equals("syp"))
+				seeYourPasswords();
+			else if(response.toLowerCase().equals("cp"))
+				changePassWord();
+			else if(response.toLowerCase().equals("cu"))
+				changeUserName();
+			else
+				System.out.println("I dont even know what you are talking about.");
+			System.exit(1);
+		}
+		
+		else {
+			System.out.println("Username or password wrong, terminating application.");
+			System.exit(000);
+		}
+	}
 
+	public static void seeYourPasswords() throws Exception{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your username again: ");
+		String userName = sc.next();
+		String hashedUserName = hashStrings(userName);
+		System.out.println("Enter your password again: ");
+		String passWord = sc.next();
+		String hashedPassWord = hashStrings(passWord);
+		if(hashedPassWord.equals(orgPassWord)&&hashedUserName.equals(orgUserName)) {
 			String basePath = "C:\\Users\\"+sysName+"\\Desktop\\OctiaxVault\\src\\base.txt";
 			ArrayList<String> fileNames = new ArrayList<String>();
 			BufferedReader br = new BufferedReader(new FileReader(basePath));
@@ -40,10 +68,9 @@ public class seeYourPasswords
 				fileNames.add(fileDecrypt(currentLine));
 			}
 			System.out.println(fileNames);
-
-			//inputting the name of the website whose password the user wants to see
 			System.out.println("Enter the name of site you want to see the password for: ");
-			String sitename = sc.nextLine();
+			Scanner s = new Scanner(System.in);
+			String sitename = s.nextLine();
 			String lowerSiteName = sitename.toLowerCase();
 			String actualSiteName = hashStrings(lowerSiteName);
 			String path = "C:\\Users\\"+sysName+"\\Desktop\\OctiaxVault\\" + actualSiteName + ".txt";
@@ -84,12 +111,8 @@ public class seeYourPasswords
 				System.exit(0);
 			}
 		}
-		
-		else {
-			System.out.println("Username or password wrong, terminating application.");
-			System.exit(000);
-		}
 	}
+
 	public static String hashStrings(String tohash) throws Exception {
 			MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
             messageDigest.update(tohash.getBytes());
@@ -133,7 +156,6 @@ public class seeYourPasswords
 	}
 
 	public static String userNameEncrypt(String toencrypt) throws Exception {
-		try {
 			IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
 			SecretKeySpec uskeySpec = new SecretKeySpec(userNameKey.getBytes("UTF-8"),"AES");
 
@@ -142,20 +164,25 @@ public class seeYourPasswords
 
 			byte[] encryptedUsername = cipher.doFinal(toencrypt.getBytes());
 			return Base64.encodeBase64String(encryptedUsername);
-		}
 
-		catch(Exception exc) {
-			exc.printStackTrace();
-		}
-		return null;
+	}
+
+	public static String passWordEncrypt(String value) throws Exception {
+		IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+		SecretKeySpec skeySpec = new SecretKeySpec(passWordKey.getBytes("UTF-8"), "AES");
+
+		Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+		cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+		byte[] encryptedPassword = cipher.doFinal(value.getBytes());
+		return Base64.encodeBase64String(encryptedPassword);
 	}
 
 	public static void changeUserName() throws Exception {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the name of the site you want to change the username or password for: ");
+		System.out.println("Enter the name of the site you want to change the username for: ");
 		String ogSiteName = sc.nextLine();
 		String hashSiteName = hashStrings(ogSiteName);
-		System.out.println(hashSiteName);
 		String path = "C://Users//"+sysName+"//Desktop//OctiaxVault//"+hashSiteName+"(@!0Zk).txt";
 		File file = new File(path);
 		if(file.exists()) {
@@ -165,10 +192,11 @@ public class seeYourPasswords
 				Scanner s = new Scanner(System.in);
 				System.out.println("Original Username: "+userNameDecrypt(fileReader.nextLine()));
 				System.out.println("ARE YOU SURE YOU WANT TO CHANGE THE USERNAME OF "+ogSiteName+"?(Y/N)");
-				String response = s.nextLine();
+				String response = s.next();
 				if(response.toLowerCase().equals("y")) {
+					Scanner scan = new Scanner(System.in);
 					System.out.println("Enter the new username for "+ogSiteName);
-					String newUserName = s.nextLine();
+					String newUserName = scan.nextLine();
 					FileWriter fw = new FileWriter(file);
 					fw.write(userNameEncrypt(newUserName));
 					fw.close();
@@ -176,6 +204,33 @@ public class seeYourPasswords
 				}
 			}
 			System.exit(0);
+		}
+	}
+
+	public static void changePassWord() throws Exception {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the name of the site you want to change the password for: ");
+		String ogSiteName = sc.nextLine();
+		String hashSiteName = hashStrings(ogSiteName);
+		String path = "C://Users//"+sysName+"//Desktop//OctiaxVault//"+hashSiteName+".txt";
+		File file = new File(path);
+		if(file.exists()) {
+			System.out.println("File found");
+			Scanner fileReader = new Scanner(file);
+			while(fileReader.hasNext()) {
+				Scanner s = new Scanner(System.in);
+				System.out.println("Original Password: "+passWordDecrypt(fileReader.nextLine()));
+				System.out.println("ARE YOU SURE YOU WANT TO CHANGE THE PASSWORD FOR "+ogSiteName+"?(Y/N)");
+				String response = s.next();
+				if(response.toLowerCase().equals("y")) {
+					System.out.println("Enter the new password for "+ogSiteName);
+					String newPassWord = s.next();
+					FileWriter fw = new FileWriter(file);
+					fw.write(passWordEncrypt(newPassWord));
+					fw.close();
+					System.out.println("Password changed succesfully! ");
+				}
+			}
 		}
 	}
 }
